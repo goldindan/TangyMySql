1b146fb29b6fa53188bab3d2ce6da279d79196cd
Dan Goldin <goldindan@gmail.com>
2018-06-28 12:05:00 +0300

    KolZhut

---
+++ Tangy/Controllers/RightsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tangy.Controllers
+{
+    public class RightsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show(string rightId)
+        {
+            return View(nameof(Show),rightId);
+        }
+    }
+}
---
+++ Tangy/Models/RightsPannelViewModel/RightsPannelViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tangy.Models.RightsPannelViewModel
+{
+    public class RightsPannelViewModel
+    {
+        public String Title { get; set; }
+        public List<String> Rights { get; set; }
+    }
+}
