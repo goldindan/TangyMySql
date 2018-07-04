2d2791fc659ecdb56bd79463a248505fcff53cb1
Dan Goldin <goldindan@gmail.com>
2018-07-04 16:01:58 +0300

    Links

---
+++ Tangy/ViewComponents/LinksViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Tangy.Data;
+
+namespace Tangy.ViewComponents
+{
+    public class LinksViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LinksViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var linksDb = await _db.LinkItem.ToListAsync();
+
+            return View(linksDb);
+        }
+
+    }
+}
