643b57ed7169a06d560a62831204c3d327db43e3
Dan Goldin <goldindan@hotmail.com>
2018-07-11 04:30:49 +0300

    Layout

---
+++ Tangy/ViewComponents/EventsViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Tangy.Data;
+
+namespace Tangy.ViewComponents
+{
+    public class EventsViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EventsViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var linksDb = await _db.Event.Where(l=>l.IsActive).OrderBy(l=>l.OrderId).ToListAsync();
+
+            return View(linksDb);
+        }
+
+    }
+}
