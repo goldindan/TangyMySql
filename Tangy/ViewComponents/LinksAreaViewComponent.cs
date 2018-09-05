7e43935e9d741187ec9f9046410c329af7f57172
Dan Goldin <goldindan@hotmail.com>
2018-09-05 07:06:13 +0300

    Update from ARG

---
+++ Tangy/ViewComponents/LinksAreaViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Tangy.Data;
+
+namespace Tangy.ViewComponents
+{
+    public class LinksAreaViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LinksAreaViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var linksDb = await _db.LinkItem.Where(l=>l.isActive).OrderBy(l=>l.OrderId).ToListAsync();
+
+            return View(linksDb);
+        }
+
+    }
+}
